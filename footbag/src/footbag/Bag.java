a4abddca2201d8439aecdb7ac6a9d1b9df7025f5
Mallimander <mallimander@gmail.com>
2016-09-14 09:28:08 -0400

    First commit

---
+++ footbag/src/footbag/Bag.java
@@ -0,0 +1,17 @@
+package footbag;
+
+
+public class Bag {
+
+	private float weight, bounce, x, y, r;
+	
+	public Bag (float weight, float bounce, float x, float y, float r){
+		this.weight = weight;
+		this.bounce = bounce;
+		this.x = x;
+		this.y = y;
+		this.r = r;
+	}
+	
+	
+}
